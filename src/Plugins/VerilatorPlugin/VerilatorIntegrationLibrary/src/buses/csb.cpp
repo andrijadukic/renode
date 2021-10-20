5d9690e22dd2ce0060cf285cc74009746fe802ac
Miskina <mhlkv02@gmail.com>
2021-10-20 18:22:24 +0200

    Added CSB bus

---
+++ src/Plugins/VerilatorPlugin/VerilatorIntegrationLibrary/src/buses/csb.cpp
@@ -0,0 +1,68 @@
+#include "csb.h"
+
+void CSB::tick(bool countEnable, uint64_t steps = 1)
+{
+    for(uint32_t i = 0; i < steps; ++i)
+    {
+        *csb_clock = 1;
+        eval();
+        *csb_clock = 0;
+        eval();
+
+    }
+
+    if (countEnable)
+    {
+        tickCounter += steps;
+    }
+}
+
+void CSB::timeoutTick(uint8_t* signal, uint8_t value, int timeout = 20)
+{
+    do
+    {
+        tick(true);
+        --timeout;
+    } while(*signal != value && timeout > 0);
+
+    if (!timeout)
+    {
+        throw "CSB Operation timeout";
+    }
+}
+
+void CSB::write(uint64_t addr, uint64_t value)
+{
+    *csb_nposted = 0;
+    *csb_valid = 1;
+    // FIXME: Ako NVDLA sere makni shiftanje?
+    *csb_addr = addr >> 2;
+    *csb_wdat = value;
+    *csb_write = 1;
+
+    timeoutTick(csb_ready, 1);
+
+    *csb_valid = 0;
+    *csb_addr = 0;
+    *csb_wdat = 0;
+}
+
+uint64_t CSB::read(uint64_t addr)
+{
+    *csb_valid = 1;
+    *csb_addr = addr;
+    *csb_write = 0;
+
+    timeoutTick(csb_ready, 1);
+
+    *csb_valid = 0;
+    *csb_addr = 0;
+    *csb_write = 0;
+
+    timeoutTick(csb_r_valid, 1);
+
+    return *csb_r_data;
+}
+
+// set all to 0?
+void CSB::reset() { }
---
+++ src/Plugins/VerilatorPlugin/VerilatorIntegrationLibrary/src/buses/csb.h
@@ -0,0 +1,33 @@
+#ifndef CSB_H_
+#define CSB_H_
+
+#include "bus.h"
+
+struct CSB : public BaseBus
+{
+    virtual void tick(bool countEnable, uint64_t steps);
+    virtual void tick(bool countEnable, uint64_t steps);
+    virtual void write(uint64_t addr, uint64_t value);
+    virtual uint64_t read(uint64_t addr);
+    virtual void reset();
+    void timeoutTick(uint8_t* signal,
+                     uint8_t expectedValue,
+                     int timeout);
+
+
+    uint8_t* csb_clock;
+    uint8_t* csb_ready;
+    uint8_t* csb_r_valid;
+    uint32_t* csb_r_data;
+    uint8_t* csb_wr_complete;
+    uint8_t* csb_valid;
+    uint16_t* csb_addr;
+    uint32_t* csb_wdat;
+    uint8_t* csb_write;
+    uint8_t* csb_nposted;
+
+
+};
+
+
+#endif // CSB_H_
